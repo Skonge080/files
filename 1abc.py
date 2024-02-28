dce1bc354d6fa508ec94747b21678b14b375418c
Skonge080 <108742702+Skonge080@users.noreply.github.com>
2024-02-28 11:49:09 +0600

    Update 1abc.py

--- 1abc.py
+++ 1abc.py
@@ -1,6 +1,22 @@
-arr = [[11, 22, 33], [44, 55, 66]]
+import random
 
-for i in range(len(arr)):
-    for j in range(len(arr[i])):
-        print("arr[", i, j, "]=", arr[i][j], end=" ")
-        print()
+arr = []
+
+for i in range(10):
+    n = round(random.random() * 100)
+    arr.append(n)
+
+print("Исходный список:", arr)
+
+lst = []
+i = 0
+
+while i < len(arr):
+    if 20 < arr[i] < 40:
+        lst.append(arr[i])
+        del arr[i]
+    else:
+        i += 1
+
+print("Список после удаления по условию:", arr)
+print("Список удаленных:", lst)
